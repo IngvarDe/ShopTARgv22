8df4dd42b983742936cc225c4a8f1d3156588cec
Ingvar Derešivski <inde999@gmail.com>
2024-01-09 09:14:38 +0200

    RegisterInProgress

--- Shop/Shop/Controllers/AccountsController.cs
+++ Shop/Shop/Controllers/AccountsController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+using Shop.Models.Accounts;
 
 namespace Shop.Controllers
 {
     public class AccountsController : Controller
     {
+
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    City = model.City,
+                };
+            }
+
             return View();
         }
     }
